3bc95c6b44298fdf46dfd252faed23a8d90334b3
Arkboi X <arkboi.x@gmail.com>
2025-02-08 19:46:47 +0530

    make sure existing backup doesnt exist

--- backup.sh
+++ backup.sh
@@ -2,6 +2,7 @@
 
 echo STARTING THE BACKUP
 cd $HOME
+rm -rf dwm-hyprland-backup
 mkdir dwm-hyprland-backup
 cp -r .config/hypr .config/waybar .config/swaync .config/rofi .config/kitty dwm-hyprland-backup
 echo
