e2a7e7dd08a8cf99bdeee0bfa8723d402851fdc9
Arkboi X <arkboi.x@gmail.com>
2025-02-09 08:55:59 +0530

    useless-ify

--- dwm-hyprland/hypr/scripts/wofi.sh
+++ dwm-hyprland/hypr/scripts/wofi.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-THEMES_DIR="$HOME/wofi-themes/my-themes"
-
-THEMES=$(find "$THEMES_DIR" -type f \( -iname "*.css" \))
-
-RANDOM_THEME=$(echo "$THEMES" | shuf -n 1)
-
-wofi -s "${RANDOM_THEME}" --show drun
+# NOT BEING USED, DELETE
