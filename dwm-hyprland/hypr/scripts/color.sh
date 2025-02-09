6f46807bd481066acb60edebcc9877c099875524
Arkboi X <arkboi.x@gmail.com>
2025-02-09 08:49:39 +0530

    make this guy useless

--- dwm-hyprland/hypr/scripts/color.sh
+++ dwm-hyprland/hypr/scripts/color.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-
-# Fetch the path of current wallpaper
-WALL=$(grep -oP '^wallpaper = eDP-1,\s*\K.+' ~/.config/hypr/hyprpaper.conf)
-
-# Set colorscheme with wal
-wal -i "$WALL"
-
-# Reload waybar
-pkill waybar
-waybar
+# Colors dot sh is not being used, delete this file
