f82f6c16ba978e3ad42c25139f818b11c3e8b997
Arkboi X <arkboi.x@gmail.com>
2025-02-09 08:54:47 +0530

    Make him useless

--- dwm-hyprland/hypr/scripts/kitty.sh
+++ dwm-hyprland/hypr/scripts/kitty.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-THEME_DIR="$HOME/kitty-themes"
-
-KITTY_THEMES=$(find "$THEME_DIR" -type f \( -iname "*.conf" \))
-
-RANDOM_THEME=$(echo "$KITTY_THEMES" | shuf -n 1)
-
-kitty -c "${RANDOM_THEME}"
+# NOT BEING USED, DELETE THIS
